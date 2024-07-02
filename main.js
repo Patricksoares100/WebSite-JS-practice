c070759d951eb7ed6d6d1d224a2a4a9407fc6ac7
Patrick Soares <soarespatrick91@gmail.com>
2024-07-02 18:53:29 +0100

    Convert celcius to fahrenheit

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-document.querySelector('#open-nav-menu').addEventListener('click', () => {
-    document.querySelector('header nav .wrapper').classList.add('nav-open');
+document.querySelector("#open-nav-menu").addEventListener("click", () => {
+  document.querySelector("header nav .wrapper").classList.add("nav-open");
 });
 
-document.querySelector('#close-nav-menu').addEventListener('click', () => {
-    document.querySelector('header nav .wrapper').classList.remove('nav-open');
+document.querySelector("#close-nav-menu").addEventListener("click", () => {
+  document.querySelector("header nav .wrapper").classList.remove("nav-open");
 });
+
+//greeting section
+
+const greetingText = "Hello, World!";
+const weatherCondition = "Sunny";
+const userLocation = "Lisbon";
+let temperature = 25;
+
+function celciusToFahrenheit(temperature) {
+  return (temperature * 9) / 5 + 32;
+}
+let weatherText = `${weatherCondition} in ${userLocation} with a temperature of ${celciusToFahrenheit(
+  temperature
+).toFixed(1)}°F`;
+document.querySelector("#greeting").innerHTML = greetingText;
+document.querySelector("p#weather").innerHTML = weatherText;
